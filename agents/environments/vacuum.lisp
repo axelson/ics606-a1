82284c199462d455095b57cef7ce130917d48b73
Jason Axelson (imac) <jason.axelson@gmail.com>
2010-03-14 13:56:56 -1000

    Started work on sensors

--- agents/environments/vacuum.lisp
+++ agents/environments/vacuum.lisp
@@ -63,3 +63,17 @@
 	'(1 0))
   (forward env agent-body))
 
+;;;; Sensor-related functions
+(defun check-sides (env agent-body)
+  (let* ((loc (object-loc agent-body))
+	 (up (list (first loc) (1+ (second loc))))
+	 (down (list (first loc) (1- (second loc))))
+	 (left (list (1- (first loc)) (second loc)))
+	 (right (list (1+ (first loc)) (second loc))))
+    (format t "direction is: ~A~%" (map 'list #'(lambda (in) (open-loc? in env)) (list up down left right)))
+    (format t "right is: ~A~%" (free-loc? right env))
+    (format t "left is: ~A~%" (free-loc? left env))))
+
+(defun open-loc? (loc env)
+  "A location is open if there is no obstacle there."
+  (not (find-object-if #'obstacle-p loc env)))

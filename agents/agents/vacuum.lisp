4b872e87c3f934af9e7799a7a9b1dca69d004280
Chris Ho <christopher.kw.ho@gmail.com>
2010-03-31 20:00:25 -1000

    All of chris' changes

--- agents/agents/vacuum.lisp
+++ agents/agents/vacuum.lisp
@@ -25,94 +25,108 @@
   occasionally turn.  Always suck when there is dirt.")
 
 
-(defstructure (jason-vacuum 
-   (:include agent
-    (program
-     #'(lambda (percept)
-         (destructuring-bind (bump dirt home directionsList dirtList) percept
-	   (read-line)
-	   ; Progam variables
-	   (progn
-	     (defvar currX 1)
-	     (defvar currY 1)
-	     (defvar choiceDir 0)
-	     (defvar amount 0)
-	     ; north (0) east (1) south (2) west (3)
-	     (defvar heading 1) ; start heading east
-	     )
+(defstructure
+    (jason-vacuum 
+     (:include
+      agent
+      (program
+       #'(lambda (percept)
+	   (destructuring-bind (bump dirt home directionsList dirtList furnitureList catList) percept
+	     (read-line)
+	     ;; If there was a bump, undo last move (if applicable)
+	     (if bump
+		 (undoLastMove))
 
-	   (updateMap percept)
-	   (updateHeading)
+	     (updateMap percept)
+	     (updateHeading)
 
-	   (format t "~%currX: ~A" currX)
-	   (format t "~%currY: ~A" currY)
-	   (format t "~%Heading: ")
-	   (cond
-	     ((eq 0 heading) (format t "North"))
-	     ((eq 1 heading) (format t "East"))
-	     ((eq 2 heading) (format t "South"))
-	     ((eq 3 heading) (format t "West"))
-	     (T (format t "INVALID")))
-	   (format t "~%")
-	   (format t "~%Output:~%")
+	     (format t "~%currX: ~A" currX)
+	     (format t "~%currY: ~A" currY)
+	     (format t "~%Heading: ")
+	     (cond
+	       ((eq 0 heading) (format t "North"))
+	       ((eq 1 heading) (format t "East"))
+	       ((eq 2 heading) (format t "South"))
+	       ((eq 3 heading) (format t "West"))
+	       (T (format t "INVALID")))
+	     (format t "~%")
+	     (format t "~%Output:~%")
 
-	   (printDamnMap map 8 12)
+	     (printDamnMap map mapY mapX)
+	     (format t "~%")
+	     (printDamnMap floodMap mapY mapX)
 
-	   ; Final action
-	   (cond
-	     (dirt (updateAction 'suck))
-	     (T (progn
-		  (setf amount 0)
-					; North
-		  (setf amountT (aref map (1+ currY) currX))
-		  (if (> amountT amount)
-		      (progn
-			(setf amount amountT)
-			(setf choiceDir 0)))
-					; East
-		  (setf amountT (aref map currY (1+ currX)))
-		  (if (> amountT amount)
-		      (progn
-			(setf amount amountT)
-			(setf choiceDir 1)))
-					; South
-		  (setf amountT (aref map (1- currY) currX))
-		  (if (> amountT amount)
-		      (progn
-			(setf amount amountT)
-			(setf choiceDir 2)))
-					; West
-		  (setf amountT (aref map currY (1+ currX)))
-		  (if (> amountT amount)
-		      (progn
-			(setf amount amountT)
-			(setf choiceDir 3)))
-		  (cond
-		    ((eq 0 choiceDir) (updateAction 'up))
-		    ((eq 1 choiceDir) (updateAction 'right))
-		    ((eq 2 choiceDir) (updateAction 'down))
-		    ((eq 3 choiceDir) (updateAction 'left))))))
-
-
-	   ;(cond (dirt (updateAction 'suck))
-		; (bump (progn (updateAction 'turnR)))
-		 ;(home (updateAction 'forward))
-                 ;(t (progn (updateAction 'up))))
-	   )))))
-		 ;;(t (random-element '(forward forward forward shut-off
-		;;			      (turn right) (turn left))))))))))
-  "A very stupid agent: ignore percept and choose a random action.")
+	     ;; Final action
+	     (if dirt
+		 (updateAction 'suck)
+		 (cond
+		   ((> plan 0) ())
+		   
+		   (T (progn
+			(setf amount 1)
+			(setf choiceDir -1)
+			;; North
+			(setf amountT (aref map (1+ currY) currX))
+			(if (> amountT amount)
+			    (progn
+			      (setf amount amountT)
+			      (setf choiceDir 0)))
+			;; East
+			(setf amountT (aref map currY (1+ currX)))
+			(if (> amountT amount)
+			    (progn
+			      (setf amount amountT)
+			      (setf choiceDir 1)))
+			;; South
+			(setf amountT (aref map (1- currY) currX))
+			(if (> amountT amount)
+			    (progn
+			      (setf amount amountT)
+			      (setf choiceDir 2)))
+			;; West
+			(setf amountT (aref map currY (1+ currX)))
+			(if (> amountT amount)
+			    (progn
+			      (setf amount amountT)
+			      (setf choiceDir 3)))
+			
+			(cond
+			  ((eq 0 choiceDir) (updateAction 'up))
+			  ((eq 1 choiceDir) (updateAction 'right))
+			  ((eq 2 choiceDir) (updateAction 'down))
+			  ((eq 3 choiceDir) (updateAction 'left))
+			  (T (progn
+			       (flood)
+			       (updateAction 'right))))
+			)))))))))
+"A very stupid agent")
 
 (defun chris-play ()
   "Reset vacuum variables and launches vacuum"
   (progn
+    (defparameter mapX 12)
+    (defparameter mapY 8)
     (defparameter currX 1)
     (defparameter currY 1)
     (defparameter heading 1)
     (defparameter choiceDir 0)
     (defparameter amount 0)
-    (defparameter map (make-array '(8 12) :initial-element 'u))
+    (defparameter plan 0)
+    (defparameter lastMove -1)
+    (defparameter map (make-array (list mapY mapX) :initial-element 0))
+    (defparameter floodMap (make-array (list mapY mapX)))
     (format t "All variables have been reset")
+    ; Create walls
+    (loop for i from 0 to (1- mapY) do
+	 (progn
+	   (setf (aref map i 0) -1)
+	   (setf (aref map i (1- mapX)) -1)))
+    (loop for i from 0 to (1- mapX) do
+	 (progn
+	   (setf (aref map 0 i) -1)
+	   (setf (aref map (1- mapY) i) -1)))
+
+    (read-a-room "a1inputspecification.txt")
     (run-environment (make-vacuum-world :aspec '(jason-vacuum)))))
 
 (defun updateHeading ()
@@ -122,45 +136,62 @@
 (defun updateMap (percept)
   "Updates map accordingly"
   (progn
-     (destructuring-bind (bump dirt home directionsList dirtList) percept
-       ; North
+     (destructuring-bind (bump dirt home directionsList dirtList furnitureList catList) percept
+       ;; North
        (setf tempDirect (car directionsList))
        (setf tempDirt (car dirtList))
+       (setf tempFurn (car furnitureList))
+       (setf tempCat (car catList))
 
-       (if (not tempDirt)
-	   (if tempDirect
-	       (setf (aref map (1+ currY) currX) 0)
-	       (setf (aref map (1+ currY) currX) -1))
-	   (setf (aref map (1+ currY) currX) 1))
+       (cond
+	 ((numberp tempDirt) (setf (aref map (1+ currY) currX) (1+ 1)))
+	 (tempFurn (setf (aref map (1+ currY) currX) -1))
+	 (tempCat (setf (aref map (1+ currY) currX) 0))
+	 (tempDirect (setf (aref map (1+ currY) currX) 1))
+	 ;; Walls
+	 (T (setf (aref map (1+ currY) currX) -1)))
 
-       ; East
+       ;; East
        (setf tempDirect (cadr directionsList))
        (setf tempDirt (cadr dirtList))
+       (setf tempFurn (cadr furnitureList))
+       (setf tempCat (cadr catList))
 
-       (if (not tempDirt)
-	   (if tempDirect
-	       (setf (aref map currY (1+ currX)) 0)
-	       (setf (aref map currY (1+ currX)) -1))
-	   (setf (aref map currY (1+ currX)) 1))
+       (cond
+	 ((numberp tempDirt) (setf (aref map currY (1+ currX)) (1+ 1)))
+	 (tempFurn (setf (aref map currY (1+ currX)) -1))
+	 (tempCat (setf (aref map currY (1+ currX)) 0))
+	 (tempDirect (setf (aref map currY (1+ currX)) 1))
+	 ;; Walls
+	 (T (setf (aref map currY (1+ currX)) -1)))
 
-       ; South
+       ;; South
        (setf tempDirect (caddr directionsList))
        (setf tempDirt (caddr dirtList))
-       (if (not tempDirt)
-	   (if tempDirect
-	       (setf (aref map (1- currY) currX) 0)
-	       (setf (aref map (1- currY) currX) -1))
-	   (setf (aref map (1- currY) currX) 1))
+       (setf tempFurn (caddr furnitureList))
+       (setf tempCat (caddr catList))
 
-       ; West
+       (cond
+	 ((numberp tempDirt) (setf (aref map (1- currY) currX) (1+ 1)))
+	 (tempFurn (setf (aref map (1- currY) currX) -1))
+	 (tempCat (setf (aref map (1- currY) currX) 0))
+	 (tempDirect (setf (aref map (1- currY) currX) 1))
+	 ;; Walls
+	 (T (setf (aref map (1- currY) currX) -1)))
+
+       ;; West
        (setf tempDirect (cadddr directionsList))
        (setf tempDirt (cadddr dirtList))
+       (setf tempFurn (cadddr furnitureList))
+       (setf tempCat (cadddr catList))
 
-       (if (not tempDirt)
-	   (if tempDirect
-	       (setf (aref map currY (1- currX)) 0)
-	       (setf (aref map currY (1- currX)) -1))
-	   (setf (aref map currY (1- currX)) 1))
+       (cond
+	 ((numberp tempDirt) (setf (aref map currY (1- currX)) (1+ 1)))
+	 (tempFurn (setf (aref map currY (1- currX)) -1))
+	 (tempCat (setf (aref map currY (1- currX)) 0))
+	 (tempDirect (setf (aref map currY (1- currX)) 1))
+	 ;; Walls
+	 (T (setf (aref map currY (1- currX)) -1)))
     )))
 
 (defun updateAction (action)
@@ -168,7 +199,10 @@
   ; Legal actions:
   ; suck forward turn (L,R) shut-off up down left right
   (cond
-    ((eq action 'suck) 'suck)
+    ((eq action 'suck) (progn
+			 (if (> 0 (aref map currY currX))
+			     (decf (aref map currY currX))) 
+			 'suck))
     ((eq action 'forward) (progn
 			    (cond
 			      ((eq heading 0) (incf currY))
@@ -186,21 +220,57 @@
     ((eq action 'shut-off) (progn
 			     (format t "shut-off")))
     ((eq action 'up) (progn
+		       (setf lastMove 0)
 		       (setf heading 0)
 		       (incf currY)
 		       'up))
     ((eq action 'down) (progn
+			 (setf lastMove 2)
 			 (setf heading 2)
 			 (decf currY)
 			 'down))
     ((eq action 'left) (progn
+			 (setf lastMove 3)
 			 (setf heading 3)
 			 (decf currX)
 			 'left))
     ((eq action 'right) (progn
+			  (setf lastMove 1)
 			  (setf heading 1)
 			  (incf currX)
 			  'right))))
+
+(defun undoLastMove ()
+  (format t "LAST MOVE UNDONE!!")
+  (cond
+    ((eq lastMove 0) (decf currY))
+    ((eq lastMove 1) (decf currX))
+    ((eq lastMove 2) (incf currY))
+    ((eq lastMove 3) (incf currX)))
+)
+
+(defun planPath ()
+  
+
+  ; Final action
+  (moveTo pX pY)
+)
+
+(defun moveTo (coorX coorY)
+
+)
+
+(defun flood ()
+  (loop for i from 0 to (1- mapY) do
+       (loop for j from 0 to (1- mapX) do
+	    (progn
+	      (setf cell (aref map i j))
+	      (if (> cell 1)
+		  (setf (aref floodMap i j) 0)
+		  (if (eq cell 1)
+		      (setf (aref floodMap i j) 9)
+		      (setf (aref floodMap i j) (* mapY mapX)))))))
+)
 
 (defun printDamnMap (map height width)
   "Prints the damn map"
